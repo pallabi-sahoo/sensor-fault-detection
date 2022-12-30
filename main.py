fff05bc3a13cfc6cc5f68754002ce4af5f040c1f
pallabi-sahoo <pallabisahoo098.com>
2022-12-31 00:39:13 +0530

    update

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import os,sys
 from sensor.logger import logging
 from sensor.pipeline import training_pipeline
 from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.utils.main_utils import read_yaml_file
 
+def set_env_variable(env_file_path):
+    env_config = read_yaml_file(env_file_path)
+    os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
 if __name__ == '__main__':
     try:
         training_pipeline = TrainPipeline()
--- sensor/data_access/sensor_data.py
+++ sensor/data_access/sensor_data.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
+import json
 
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.constant.database import DATABASE_NAME
@@ -20,6 +21,20 @@ class SensorData:
         try:
             self.mongo_client = MongoDBClient(database_name=DATABASE_NAME)
 
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    def save_csv_file(self,file_path ,collection_name: str, database_name: Optional[str] = None):
+        try:
+            data_frame=pd.read_csv(file_path)
+            data_frame.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data_frame.T.to_json()).values())
+            if database_name is None:
+                collection = self.mongo_client.database[collection_name]
+            else:
+                collection = self.mongo_client[database_name][collection_name]
+            collection.insert_many(records)
+            return len(records)
         except Exception as e:
             raise SensorException(e, sys)
 
