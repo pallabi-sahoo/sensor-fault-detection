61b25d1745085190dde9ffc2c77606ae5d000b14
pallabi-sahoo <pallabisahoo098.com>
2022-12-31 00:02:37 +0530

    update

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,6 +1,8 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
+from sensor.constant.env_variable import MONGODB_URL_KEY
 import certifi
+import os
 ca = certifi.where()
 
 class MongoDBClient:
